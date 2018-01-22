85da5d34a21cd2cc736adb5b0aaed217c224e75d
Managersky <witoldmetel@gmail.com>
2018-01-22 09:51:50 +0100

    Pętla w pętli - tabliczka mnożenia

--- js/script.js
+++ js/script.js
@@ -1,26 +1,49 @@
-/*
+// /*
 
-arguments Object 
+// arguments Object 
 
-*/
+// */
 
-function addNumbers() 
-{   
-    var suma = 0;
+// function addNumbers() 
+// {   
+//     var suma = 0;
     
-    /*for (i = 0; i < arguments.length; i++)
-    {
-        suma += arguments[i];
-    }
-    */
+//     /*for (i = 0; i < arguments.length; i++)
+//     {
+//         suma += arguments[i];
+//     }
+//     */
 
-    for (var property in arguments)
+//     for (var property in arguments)
+//     {
+//         suma += arguments[property];
+//     }
+//     return suma;
+// }
+
+// var suma = addNumbers(3,4,7,6,6);
+
+// alert(suma);
+
+/*
+    pętla w pętli - tabliczka mnożenia ćwiczenie
+ */
+
+var tabliczkaMnozenia ="<table>";
+
+for (var i = 0; i <= 10; i++)
+{   
+    tabliczkaMnozenia += "<tr>";
+
+    for (var j = 0; j <= 10; j++)
     {
-        suma += arguments[property];
+        tabliczkaMnozenia += "<td>" + i * j + "</td>";
     }
-    return suma;
+    tabliczkaMnozenia += "</tr>";
 }
 
-var suma = addNumbers(3,4,7,6,6);
+tabliczkaMnozenia += "</table>";
 
-alert(suma);
+var rezultat = document.getElementById("rezultat");
+
+rezultat.innerHTML = tabliczkaMnozenia;
