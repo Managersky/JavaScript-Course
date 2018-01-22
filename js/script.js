b2f0df92de4fd0b51969e8b1bc51db7ec88e43cd
Managersky <witoldmetel@gmail.com>
2018-01-22 11:10:10 +0100

    Zwieksz/Zmniejsz Czcionkę

--- js/script.js
+++ js/script.js
@@ -29,21 +29,35 @@
     pętla w pętli - tabliczka mnożenia ćwiczenie
  */
 
-var tabliczkaMnozenia ="<table>";
+// var tabliczkaMnozenia ="<table>";
 
-for (var i = 0; i <= 10; i++)
-{   
-    tabliczkaMnozenia += "<tr>";
+// for (var i = 0; i <= 10; i++)
+// {   
+//     tabliczkaMnozenia += "<tr>";
 
-    for (var j = 0; j <= 10; j++)
-    {
-        tabliczkaMnozenia += "<td>" + i * j + "</td>";
-    }
-    tabliczkaMnozenia += "</tr>";
+//     for (var j = 0; j <= 10; j++)
+//     {
+//         tabliczkaMnozenia += "<td>" + i * j + "</td>";
+//     }
+//     tabliczkaMnozenia += "</tr>";
+// }
+
+// tabliczkaMnozenia += "</table>";
+
+// var rezultat = document.getElementById("rezultat");
+
+// rezultat.innerHTML = tabliczkaMnozenia;
+
+var tekst = document.getElementById("tekst");
+var plus = document.getElementById("plus");
+var minus = document.getElementById("minus");
+
+plus.onclick = function() 
+{
+    tekst.className = "zwiekszCzcionke";
 }
 
-tabliczkaMnozenia += "</table>";
-
-var rezultat = document.getElementById("rezultat");
-
-rezultat.innerHTML = tabliczkaMnozenia;
+minus.onclick = function() 
+{
+    tekst.className = "zmniejszCzcionke";
+}
