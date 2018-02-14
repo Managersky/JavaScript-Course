234aa749553c209b7981ad90def86b989a1ba075
Managersky <witoldmetel@gmail.com>
2018-02-14 15:22:49 +0100

    To Do App - Training

--- Aplication_ToDo/js/ToDo.js
+++
@@ -1,8 +0,0 @@
-
-function ToDo(description) {
-    this.id = ToDo.UID++;
-    this.description = description;
-    this.isCompleted = false;
-}
-
-ToDo.UID = 1;
---
+++ Aplication_ToDo/js/script.js
@@ -0,0 +1,10 @@
+let todoList = null;
+let todoForm = null;
+let todoSearch = null;
+
+document.addEventListener('DOMContentLoaded', function () {
+    todoList = document.querySelector('#todoList');
+    todoForm = document.querySelector('#todoForm');
+    todoSearch = document.querySelector('#todoSearch');
+
+});
