bee806d69ed7f3880a012a1413a2b8bafee46946
Managersky <witoldmetel@gmail.com>
2018-03-21 12:34:16 +0100

    mousemove shadow

---
+++ MouseMove_Text_Shadow/js/script.js
@@ -0,0 +1,28 @@
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+const walk = 300; //300px
+
+function shadow(e) {
+    // const width = hero.offsetWidth;
+    // const height = hero.offsetHeight;
+    const { offsetWidth: width, offsetHeight: height } = hero; //diffrent possibility
+    let { offsetX: x, offsetY: y } = e;
+
+    // console.log(this, e.target);
+    if (this !== e.target) { // warunek powoduje ze po najechaniu na e.target pozycja myszki sie nie zeruje
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / height * walk) - (walk / 2));
+    // console.log(xWalk, yWalk);
+    text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, 0.7),
+    ${xWalk * -1}px ${yWalk}px 0 rgba(0, 255, 255, 0.7),
+    ${xWalk}px ${yWalk * -1}px 0 rgba(0, 255, 0, 0.7),
+    ${xWalk * -1}px ${yWalk * -1}px 0 rgba(255, 255, 0, 0.7)
+    `;
+}
+
+hero.addEventListener("mousemove", shadow);
