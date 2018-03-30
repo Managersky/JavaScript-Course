a8cc031edb393880f65d5f896296427e5a94e1df
Managersky <witoldmetel@gmail.com>
2018-03-30 15:02:58 +0200

    Add timer and didable btn

--- Whack a Mole/js/script.js
+++ Whack a Mole/js/script.js
@@ -1,9 +1,11 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+const timerDisplay = document.querySelector(".display__time-left");
 let lastHole; // flag to don't repeat random hole
 let timeUp = false;
 let score = 0;
+let countdown;
 
 function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -34,7 +36,11 @@ function startGame() {
     timeUp = false;
     score = 0;
     displayMole();
-    setTimeout(() => timeUp = true, 10000)
+    timer(10);
+    setTimeout(() => {
+        timeUp = true;
+        startBtn.disabled = false;
+    }, 10000);
 }
 
 function countMole(e) {
@@ -43,6 +49,37 @@ function countMole(e) {
     this.parentNode.classList.remove('up');
     scoreBoard.textContent = score;
 }
+
+function timer(seconds) {
+    //clear any existing timers
+    clearInterval(countdown);
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds); // start countdown
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round(then - Date.now()) / 1000;
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const remainderSeconds = Math.floor(seconds % 60);
+    const display = `0:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
+}
+
 moles.forEach(mole => mole.addEventListener('click', countMole));
 
-startBtn.addEventListener("click", startGame);
+startBtn.addEventListener("click", function() {
+    if (this.disabled == false) {
+        this.disabled = true;
+        startGame();
+        console.log("sdasdsa");
+    }
+});
