64322aef6787a6126bbc6756a3a069ad6e53b9a0
witoldmetel <witoldmetel@gmail.com>
2019-01-26 14:01:57 +0100

    update + init cardio 2

--- JS Cardio/Session 1/index.js
+++ JS Cardio/Session 1/index.js
@@ -3,41 +3,41 @@
 // ex. reverseString('hello') === 'olleh'
 
 function reverseString(str) {
-    // METHOD 1
+    // SOLUTION 1
     // return str
     // .split('') // turns string into array, as parameter takes separator
     // .reverse()
     // .join(''); // turns array into string
 
-    // METHOD 2
+    // SOLUTION 2
     // let revStr = '';
     // for (let i = str.length - 1; i >= 0; i--) {
     //     revStr = revStr + str[i];
     // }
     // return revStr;
 
-    // METHOD 3
+    // SOLUTION 3
     // let revStr = '';
     // for (let i = 0; i <= str.length - 1; i++) {
     //     revStr = str[i] + revStr;
     // }
     // return revStr;
 
-    // METHOD 4
+    // SOLUTION 4
     // let revStr = '';
     // for (let char of str) { // for..of method
     //     revStr = char + revStr; // char becomes "H", then "e", then "l" etc)
     // }
     // return revStr;
 
-    // METHOD 5
+    // SOLUTION 5
     // let revStr = '';
     // str.split('').forEach(char => {
     //     revStr = char + revStr;
     // });
     // return revStr;
 
-    // METHOD 6
+    // SOLUTION 6
     return str.split('').reduce((separator, char) => char + separator, '')
 }
 
@@ -47,7 +47,7 @@ function reverseString(str) {
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
 function isPalindrome(str) { 
-    // METHOD 1
+    // SOLUTION 1
     const revString = str.split('').reverse().join('');
 
     return revString === str;
@@ -60,7 +60,7 @@ function isPalindrome(str) {
 // ex. reverseInt(521) === 125
 
 function reverseInt(int) {
-    // METHOD 1
+    // SOLUTION 1
     const convertInt = int.toString().split('').reverse().join('');
 
     return parseInt(convertInt);
@@ -72,7 +72,7 @@ function reverseInt(int) {
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 function capitalizeLetters(str) { 
-    // METHOD 1
+    // SOLUTION 1
     // const strArr = str.toLowerCase().split(' '); // make sure that all letters are small
 
     // for (let i = 0; i < strArr.length; i++) {
@@ -81,7 +81,7 @@ function capitalizeLetters(str) {
     // }
     // return strArr.join(' ');
 
-    // METHOD 2
+    // SOLUTION 2
     // return str
     //     .toLowerCase()
     //     .split(' ')
@@ -89,7 +89,7 @@ function capitalizeLetters(str) {
     //     .map(word => word[0].toUpperCase() + word.substr(1))
     //     .join(' ');
 
-    // METHOD 3
+    // SOLUTION 3
     const regExp = /\b[a-z]/gi;
 
     return str.replace(regExp, char => char.toUpperCase());
@@ -101,7 +101,7 @@ function capitalizeLetters(str) {
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
 function maxCharacter(str) { 
-    // METHOD 1
+    // SOLUTION 1
     const charMap = {};
     let maxNum = 0;
     let maxChar = '';
@@ -128,7 +128,7 @@ function maxCharacter(str) {
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 function fizzBuzz() {
-    // METHOD 1
+    // SOLUTION 1
     for (let i = 0; i <= 100; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log('FizzBuzz');
@@ -141,8 +141,6 @@ function fizzBuzz() {
         }
     }
  }
-
-
 
 // Call Function
 const output = fizzBuzz();
