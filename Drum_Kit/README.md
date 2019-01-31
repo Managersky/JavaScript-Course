1b8efab80c690406a6fd9b54bd9a69233e8d5f50
witoldmetel <witoldmetel@gmail.com>
2019-01-31 19:56:53 +0100

    Transfer challange 1 to JS Course

---
+++ Drum_Kit/README.md
@@ -0,0 +1,13 @@
+# Drum-Kit #JavaScript30
+
+## Challenge 1
+
+## What I learned?
+
+1) audio tag
+2) data-key, keyCode
+3) template strings
+
+## Live: 
+
+https://witoldmetel.github.io/Drum-Kit/
---
+++ Drum_Kit/js/script.js
@@ -0,0 +1,31 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return //stop the function from running
+
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+    key.classList.add("playing");
+};
+
+function playSoundClick() {
+    const key = this.getAttribute("data-key");
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+    this.classList.add("playing");
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return // skip it if is not transform
+    this.classList.remove("playing");
+};
+
+window.addEventListener("keydown", playSound);
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach(key => key.addEventListener("click", playSoundClick));
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
